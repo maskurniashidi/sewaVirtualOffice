5a48008bcc3a629e2e74cf7ddcf9e930908913b6
syiida <rosyiidahasnaa@gmail.com>
2022-02-25 10:17:50 +0700

    service controller

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Service;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $services = Service::all(); 
+        return $services; 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $service = Service::findOrFail($id);
+        return $service;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name'=>'required',
+            'cost'=>'required',
+            'image_url'=>'required',
+            'space'=>'required',
+            'capacity'=>'required',
+            'description',
+         ]);
+               
+         $post = Post::find($id)->update($request->all()); 
+                
+         return back()->with('success',' Data telah diperbaharui!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- database/migrations/2022_02_24_065234_create_services_table.php
+++ database/migrations/2022_02_24_065234_create_services_table.php
@@ -15,6 +15,12 @@ class CreateServicesTable extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->double('cost', 12, 2);
+            $table->string('image_url');
+            $table->double('space', 8, 2);
+            $table->integer('capacity');
+            $table->text('description');
             $table->timestamps();
         });
     }
