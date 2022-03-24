c8336d6cb32254f6f1d97d23277cfb86b18070e2
syiidahasnaa30 <syiidahasnaa30@gmail.com>
2022-03-24 09:12:18 +0700

    add login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Helpers\ResponseFormatter;
 
@@ -26,5 +27,30 @@ class AuthController extends Controller
     }
     public function login(Request $request)
     {
+        $validate = $request->validate([
+            "email" => 'required|email:dns',
+            'password' => 'required|min:8|max:255'
+        ]);
+        //return $validate;
+        if (Auth::attempt($validate)) {
+            $user = User::where('email', $request->email)->first();
+            $token = $user->createToken('access_token')->plainTextToken;
+            return ResponseFormatter::success([
+                'token' => $token,
+                'user' => $user
+            ], 'Login Success');
+        } else {
+            return ResponseFormatter::error(null, 'Login failed');
+        }
+    }
+    public function logout(Request $request)
+    {
+
+        $request->user()->tokens()->delete();
+        if (Auth::user()->current) {
+            return ResponseFormatter::error(null, 'Something went wrong', 404);
+        }
+        Auth::logout();
+        return ResponseFormatter::success(null, 'Logout successful');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::post('/service', [ServiceController::class, 'store']);
 Route::get('/service', [ServiceController::class, 'index']);
