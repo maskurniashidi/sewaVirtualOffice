4334e9da949345abeb73310d53fac6e564978963
syiidahasnaa30 <syiidahasnaa30@gmail.com>
2022-04-03 11:25:37 +0700

    fix csrf problem

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,20 +16,20 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8'
         ]);
-        if($validator->fails()){
-            return response()->json($validator->errors());       
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
         }
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'is_admin' => 0,
-         ]);
+        ]);
 
         return ResponseFormatter::success(
             $user,
@@ -39,20 +39,20 @@ class AuthController extends Controller
 
     public function registerAdmin(Request $request)
     {
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8'
         ]);
-        if($validator->fails()){
-            return response()->json($validator->errors());       
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
         }
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'is_admin' => 1,
-         ]);
+        ]);
 
         return ResponseFormatter::success(
             $user,
@@ -61,26 +61,24 @@ class AuthController extends Controller
     }
     public function login(Request $request)
     {
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'email' => 'required|string|email|max:255',
             'password' => 'required|string|min:8'
         ]);
-        if($validator->fails()){
-            return response()->json($validator->errors());       
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
         }
-        if (!Auth::attempt($request->only('email', 'password')))
-        {
+        if (!Auth::attempt($request->only('email', 'password'))) {
             return response()
                 ->json(['message' => 'Unauthorized'], 401);
         }
         $user = User::where('email', $request['email'])->firstOrFail();
         $token = $user->createToken('auth_token')->plainTextToken;
         return ResponseFormatter::success([
-            'token' => "Bearer ".$token,
+            'token' => "Bearer " . $token,
             'token_type' => 'Bearer',
             'user' => $user
         ], 'Login Success');
-       
     }
     public function logout(Request $request)
     {
